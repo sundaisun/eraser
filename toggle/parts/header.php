e1bf8183172ec1eaa32f3d1bfad37d4d6cdb1ad2
Sun Dai <sundaisf@gmail.com>
2015-01-20 15:16:28 -0800

    moved Toggle nav files to toggle folder, nav now should use full srn nav overlay

---
+++ toggle/parts/header.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="stylesheets/base.css" />
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+
+<body>
+   <div class="wrapper">
+    <nav class="top-bar show-for-large-up" data-topbar>
+        <ul class="title-area">
+            <li class="name logo">
+                <h1><a href="#"></a></h1>
+            </li>
+        </ul>
+        <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul class="right">
+                <li><a href="/html/english/index.html" target="_self">Home</a>
+                </li>
+                <li><a href="/html/english/OurFoodYourQuestions/index.html" target="_self">Our food your question</a>
+                </li>
+                <li class="has-dropdown">
+                    <a href="#">In Your Community</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Education Workshop</a>
+                        </li>
+                        <li><a href="#">Family Pavillion Tent</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="/html/english/asian_phrases/index.html" target="_self">Asian Pharases Challenge</a>
+                </li>
+                <li class="has-dropdown">
+                    <a href="#">Languages</a>
+                    <ul class="dropdown">
+                        <li><a href="#">English</a>
+                        </li>
+                        <li><a href="#">中文</a>
+                        </li>
+                        <li><a href="#">한국어</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+    </nav>
+    <!--end show for large up-->
+
+    <!-- wrap all content with off-canvas-->
+    <div class="off-canvas-wrap" data-offcanvas>
+        <div class="inner-wrap">
+            <nav class="tab-bar hide-for-large-up">
+                    <div class="name logo">
+                        <h1><a href="#"></a></h1>
+                    </div>
+                <section class="right-small"> <a id="toggle" class="right-off-canvas-toggle" href="#"><span></span></a> 
+                </section>
+            </nav>
+            <!--tab bar-->
+            <aside class="right-off-canvas-menu" id="menu">
+                <ul class="off-canvas-list">
+                    <li><a href="/html/english/index.html" target="_self">Home</a>
+                    </li>
+                    <li><a href="/html/english/OurFoodYourQuestions/index.html" target="_self">Our food your question</a>
+                    </li>
+                    <li class="has-submenu"><a href="#" target="_self">In your community</a>
+                        <ul class="right-submenu">
+                            <li class="back"><a href="#">Back</a>
+                            </li>
+                            <li><a href="#">Education Workshop</a>
+                            </li>
+                            <li><a href="#">Family Pavillion Tent</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <li><a href="/html/english/asian_phrases/index.html" target="_self">Asian Pharases Challenge</a>
+                    </li>
+                    <li class="has-submenu"><a href="#" target="_self">Languages</a>
+                        <ul class="right-submenu">
+                            <li class="back"><a href="#">Back</a>
+                            </li>
+                            <li><a href="#">English</a>
+                            </li>
+                            <li><a href="#">中文</a>
+                            </li>
+                            <li><a href="#">한국어</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </aside>
